4c43deca0ee34fd914ea835f25c99271e72f12ef
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-06-12 23:06:02 +0800

    to master

--- API/database/migrations/2024_06_12_134550_create_user_roles_table.php
+++ API/database/migrations/2024_06_12_134550_create_user_roles_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_roles', function (Blueprint $table) {
-            $table->id();
+            $table->foreignId('userID')->constrained('users')->onDelete('cascade');
+            $table->foreignId('roleID')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }

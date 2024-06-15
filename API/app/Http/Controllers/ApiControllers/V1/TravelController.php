3d30431974db59676470547c5806c7aa77fb5912
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-06-15 14:38:38 +0800

    tyo master

--- API/app/Http/Controllers/ApiControllers/V1/TravelController.php
+++ API/app/Http/Controllers/ApiControllers/V1/TravelController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\ApiControllers\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Travel;
 
 class TravelController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return Travel::where('is_public', true)->get();
+    }
+
 }

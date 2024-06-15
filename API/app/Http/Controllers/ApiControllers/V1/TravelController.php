a84276348d235bdf8afed8b0864d1f6ea33b55bc
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-06-15 22:03:54 +0800

    added controllers

--- API/app/Http/Controllers/ApiControllers/V1/TravelController.php
+++ API/app/Http/Controllers/ApiControllers/V1/TravelController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers\ApiControllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Travel;
+use Illuminate\Support\Facades\Request;
 
 class TravelController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        return Travel::where('is_public', true)->get();
+        $data = Travel::select('*')->where('is_public', true)->get();
+        return $data;
     }
+
 
 }
--- API/routes/web.php
+++ API/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ApiControllers\V1\TravelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('v1/travel', [TravelController::class, 'index']);

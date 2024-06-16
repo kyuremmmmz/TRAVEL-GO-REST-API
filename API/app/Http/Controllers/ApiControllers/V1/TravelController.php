7959168488b71b72594b5ab46544267fdcfff4e0
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-06-16 16:16:54 +0800

    controller of tours

--- API/app/Http/Controllers/ApiControllers/V1/TravelController.php
+++ API/app/Http/Controllers/ApiControllers/V1/TravelController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\ApiControllers\V1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\TourResource;
+use App\Http\Resources\TravelResource;
 use App\Models\Travel;
 use Illuminate\Support\Facades\Request;
 
@@ -13,7 +13,7 @@ class TravelController extends Controller
     public function index(Request $request)
     {
         $data = Travel::where('is_public', true)->paginate();
-        return TourResource::collection($data);
+        return TravelResource::collection($data);
     }
 
 
--- API/routes/web.php
+++ API/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiControllers\V1\TourController;
 use App\Http\Controllers\ApiControllers\V1\TravelController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::get('v1/travel', [TravelController::class, 'index'])->name('travel');
+Route::get('travel/{travel:slug}/tours', [TourController::class, 'index']);

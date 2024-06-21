64f0cdec4b8c7dd5a41663363079d7680e4190b2
Christian David Jasmin <kurosawataki84@gmail.com>
2024-06-21 20:37:47 +0800

    to master

--- API/routes/web.php
+++ API/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiControllers\V1\ApiCreatorController;
 use App\Http\Controllers\ApiControllers\V1\TourController;
 use App\Http\Controllers\ApiControllers\V1\TravelController;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,5 @@ Route::get('/', function () {
 
 Route::get('v1/travel', [TravelController::class, 'index'])->name('travel');
 Route::get('v1/travel/{travel:slug}/Tour', [TourController::class, 'index']);
+Route::get('travel/travelCreator', [ApiCreatorController::class, 'view'])->name('view');
+Route::post('v1/ApiCreatorController', [ApiCreatorController::class, 'createTours'])->name('Create');

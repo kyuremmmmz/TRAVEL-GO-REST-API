fc796e5c4124a6d395c8aa06455930b9c8828963
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-06-22 18:25:03 +0800

    to master

--- API/app/Http/Controllers/ApiControllers/V1/ApiCreatorController.php
+++ API/app/Http/Controllers/ApiControllers/V1/ApiCreatorController.php
@@ -3,23 +3,29 @@
 namespace App\Http\Controllers\ApiControllers\V1;
 
 use App\Http\Controllers\Controller;
-use App\Models\tours;
+use App\Models\Travel;
 use Illuminate\Http\Request;
 
 class ApiCreatorController extends Controller
 {
-    public function createTours(Request $request, tours $tours)
+    public function createTours(Request $request, Travel $travel)
     {
-        $tour = $request->validate([
-            'name' =>'name',
-            'description' =>'description',
-            'price' =>'price',
-            'is_public' =>'required|boolean|true',
+        $travels = $request->validate([
+            'name' =>'required|string',
+            'description' =>'required|string',
+            'price' =>'required|integer',
+            'number_of_days'=>'required|integer',
+            'is_public' =>'required|boolean',
         ]);
 
-        $tours->create($tour);
+        $travel->create($travels);
 
-        return dd($request->$tours->orderBy('name', 'asc')->get());
+        return redirect(route('travel'));
 
+    }
+
+    public function view()
+    {
+        return view('travel/travelCreator');
     }
 }

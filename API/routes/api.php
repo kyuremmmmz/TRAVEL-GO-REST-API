bc5c6c0922cb58d3dbcb6b88632564c6227826d3
Christian David Jasmin <kurosawataki84@gmail.com>
2024-06-30 18:38:55 +0800

    just nilipat

--- API/routes/api.php
+++ API/routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ApiControllers\V1\ApiCreatorController;
+use App\Http\Controllers\ApiControllers\V1\Auth\LoginController;
+use App\Http\Controllers\ApiControllers\V1\TourController;
 use App\Http\Controllers\ApiControllers\V1\TravelController;
+use App\Http\Controllers\ApiControllers\V1\ViewTable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,22 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('v1/travel', [TravelController::class, 'index'])->name('travel');
+Route::get('v1/travel/{travel:slug}/Tour', [TourController::class, 'index']);
+Route::get('travel/travelCreator', [ApiCreatorController::class, 'view'])->name('view');
+Route::get('tablesView/table', [ViewTable::class,'index'])->name('tableview');
+Route::delete('tablesView/table/{travel}', [ViewTable::class, 'Delete'])->name('destroy');
+
+
+
+
+
+Route::post('travels', [TravelController::class, 'createTours'])->name('Create');
+Route::post('login', [LoginController::class, '__invoke'])->name('login');
+Route::post('v1/travel/TourController', [TourController::class, 'store'])->name('ToursCreate');
+Route::get('Tour/TourResources', [TourController::class, 'Tignan'])->name('view');
+Route::post('v1/ApiControllers/TravelController/{travel:name}', [TravelController::class, 'update'])->name('Update');
+
 
 
 

aef5ec86c0e02bf8cd205cd2a9e4a68f6c9b63ec
Christian David Jasmin <kurosawataki84@gmail.com>
2024-07-04 20:42:33 +0800

    hahahahah

--- API/app/Http/Controllers/ApiControllers/V1/Auth/LoginController.php
+++ API/app/Http/Controllers/ApiControllers/V1/Auth/LoginController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\ApiControllers\V1\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginRequest;
-use Illuminate\Foundation\Auth\User;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
@@ -12,30 +12,22 @@ class LoginController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function csrf()
-    {
-        return csrf_token();
-    }
-
-    public function __invoke(LoginRequest $request, $id)
+    public function __invoke(Request $request, User $user)
     {
 
-        $credentials = User::where('email', $request->email)->get();
+        $credentials = User::where('email', $request->email)->first();
 
         if (! $credentials || ! Hash::check($credentials->password, $credentials->password)) {
             return response()->json([
-                'error' => 'Credentials do not match'
-            ])->setStatusCode(422);
+                'error' => 'credentials doesnt match'
+            ])->setStatusCode(400);
 
         }
 
         $device = substr($request->userAgent() ??'',0,255);
-        $csrf = csrf_token();
-        $accesToken = $credentials->createToken($device)->plainTextToken;
+
         return response()->json([
-            'csrf' =>  $csrf,
-            'data' => $credentials,
-            'access_token'=> $accesToken,
-        ])->setStatusCode(200);
+            'access_token'=> $user->createToken($device)->plainTextToken,
+        ]);
     }
 }

02c9552428558ffa2e0c901411b72b215e062c2b
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-07-05 19:47:16 +0800

    to master

--- API/app/Http/Controllers/ApiControllers/V1/Auth/LoginController.php
+++ API/app/Http/Controllers/ApiControllers/V1/Auth/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\ApiControllers\V1\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Foundation\Auth\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
@@ -14,7 +13,7 @@ class LoginController extends Controller
      * Handle the incoming request.
      */
 
-    public function __invoke(LoginRequest $request, Request $requestt)
+
 
     public function csrf()
     {
@@ -22,6 +21,7 @@ class LoginController extends Controller
     }
 
     public function __invoke(LoginRequest $request, $id)
+
     {
 
         $credentials = User::where('email', $request->email)->get();
@@ -34,17 +34,8 @@ class LoginController extends Controller
         }
 
         $device = substr($request->userAgent() ??'',0,255);
-        $csrf = csrf_token();
-        $accesToken = $credentials->createToken($device)->plainTextToken;
         return response()->json([
-
             'access_token'=> $credentials->createToken($device)->plainTextToken,
             ])->setStatusCode(200);
-
-            'csrf' =>  $csrf,
-            'data' => $credentials,
-            'access_token'=> $accesToken,
-        ])->setStatusCode(200);
->>>>>>> d15075b4e06acb5a6df981cf85da664335a3133c
     }
 }

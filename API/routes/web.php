0955dd8356c4aa469bf0026fd8ba2e846659abe4
Christian David Jasmin <kurosawataki84@gmail.com>
2024-07-06 20:32:47 +0800

    laravel api

--- API/routes/web.php
+++ API/routes/web.php
@@ -11,3 +11,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('v1/travel', [TravelController::class, 'index'])->name('travel');
+Route::get('v1/travel/{travel:slug}/Tour', [TourController::class, 'index']);
+Route::get('travel/travelCreator', [ApiCreatorController::class, 'view'])->name('view');
+Route::get('tablesView/table', [ViewTable::class,'index'])->name('tableview');
+Route::delete('tablesView/table/{travel}', [ViewTable::class, 'Delete'])->name('destroy');
+
+
+
+Route::post('travels', [TravelController::class, 'createTours'])->name('Create')->middleware('auth:sanctum');
+
+Route::post('login', [LoginController::class, '__invoke']);
+Route::get('index/csrftoken', [LoginController::class, 'csrf']);
+Route::post('v1/travel/TourController', [TourController::class, 'store'])->name('ToursCreate');
+Route::get('Tour/TourResources', [TourController::class, 'Tignan'])->name('view');
+Route::post('v1/ApiControllers/TravelController/{travel:name}', [TravelController::class, 'update'])->name('Update');
+Route::get('TourTables/TourTable', [TourController::class, 'tables'])->name('viewTables');
